b05ed19a4c240b7cfad30886b8aab9231f5dd59c
SantiagoGalindoHernandez19 <124641609+SantiagoGalindoHernandez19@users.noreply.github.com>
2023-05-06 17:35:28 -0500

    Add files via upload

---
+++ Punto 3, taller 2.py
@@ -0,0 +1,10 @@
+
+
+
+a = int(input("ingresa un numero entero: ")) #establecemos las entradas de los dos numeros enteros 
+b = int(input("ingresa otro numero entero: "))
+if a == b:#establecemos que dos numeros espejos son dos numeros iguales
+    print("tus dos numeros son espejos") 
+else:
+    print("tus dos numeros no son espejo")
+#propiciamos la respuesta
